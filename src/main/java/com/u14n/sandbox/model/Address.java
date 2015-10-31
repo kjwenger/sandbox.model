3feafff1bf6f5697bbd0c71a782f7b80f105ba52
Klaus Wenger <kjwenger@yahoo.com>
2015-10-30 22:11:17 -0400

    serialization play

--- src/main/java/com/u14n/sandbox/model/Address.java
+++ src/main/java/com/u14n/sandbox/model/Address.java
@@ -1,10 +1,17 @@
 package com.u14n.sandbox.model;
 
+import java.io.Serializable;
+
 /**
  * @author Klaus Wenger
  *
  */
-public class Address extends Location {
+public class Address extends Location implements Serializable {
+	/**
+	 * 
+	 */
+	//                                           major minor micro build
+	private static final long serialVersionUID = 0x0001_0000_0000_0000L;
 
 	public Address(String zipCode,
 			String additional) {
--- src/test/resources/com/u14n/sandbox/model/address.feature
+++ src/test/resources/com/u14n/sandbox/model/address.feature
@@ -1,7 +1,13 @@
 Feature: Address
 
   @noignore
-  Scenario: a address
+  Scenario: A new address
     Given I have a new address
     When I get the additional
     Then the additional should be blank
+
+  Scenario: A serialized address
+    Given I have a new address
+    When I get the serial version UID
+    Then serial version UID should be 1.0.0.0
+    
